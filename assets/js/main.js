b24604404201bf5269188fa5475e2f013cea3190
LinaRedfox <vdovichenko.lv@mail.ru>
2024-01-21 04:15:26 +0300

    main changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,21 +24,57 @@ function linkAction() {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () => {
+/*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () => {
     const header = document.getElementById('header')
-    this.scrollY >= 50 ? header.classList.add('bg-header')
-        : header.classList.remove('bg-header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+        : header.classList.remove('blur-header')
 }
 
-window.addEventListener('scroll', scrollHeader)
-/*=============== ADD BLUR TO HEADER ===============*/
+window.addEventListener('scroll', blurHeader)
 
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 300 ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll')
+}
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance:'60px',
+    duration: 3000,
+    delay: 400,
+    reset: true,   //Animations repeat 
+})
+
+sr.reveal('.home_data, .explore_data, .explore_user, .footer_container')
+sr.reveal('.home_card', {delay: 600, distance: '100px', interval: 200})
+sr.reveal('.about_image, .join_data', {origin: 'left'})
+sr.reveal('.about_data,.join_image', {origin:'right'})
+sr.reveal('.popular_card',{interval: 150})
+
